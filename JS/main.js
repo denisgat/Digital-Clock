e789bc46f0fc9ff1e8201e84b8f1fd7602475d88
denisgat <denisgatotho@gmail.com>
2020-02-19 16:28:41 -0500

    variable inclusion and styling

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+let time = document.getElementById("clock");
+
+
+function ftime(){
+    let now = new Date()
+    let hours = now.getHours() ;
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    console.log(hours);
+    console.log(minutes);
+    console.log(seconds);
+}
+
+setInterval(ftime,1000);
+
+return hours 
+return minutes
+return seconds
+
+    
