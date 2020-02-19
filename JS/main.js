751375d2e30a44b21b3ed89ac11d9ac37b88e461
denisgat <denisgatotho@gmail.com>
2020-02-19 16:43:25 -0500

    main element centering

--- JS/main.js
+++ JS/main.js
@@ -6,15 +6,15 @@ function ftime(){
     let hours = now.getHours() ;
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
-    console.log(hours);
-    console.log(minutes);
-    console.log(seconds);
+
+    time.innerHTML = hours + ":"  + minutes + ":" + seconds
+    return hours 
+    return minutes
+    return seconds
 }
 
 setInterval(ftime,1000);
 
-return hours 
-return minutes
-return seconds
+
 
     
