b4182bd48c7e76b3eb7eb86ea3037ea664459683
denisgat <denisgatotho@gmail.com>
2020-02-20 19:52:30 -0500

    added double digits

--- JS/main.js
+++ JS/main.js
@@ -2,15 +2,13 @@ let time = document.getElementById("clock");
 
 
 function ftime(){
-    let now = new Date()
+    let now = new Date();
     let hours = now.getHours() ;
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
 
-    time.innerHTML = hours + ":"  + minutes + ":" + seconds
-    return hours 
-    return minutes
-    return seconds
+    time.innerHTML = ("0" + hours).slice(-2) + ":"  + ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2)
+
 }
 
 setInterval(ftime,1000);
